4c55950faaef803f0c34f4a4c371c3721fe6586e
koki.ando <koki.ando@CS-506.CSRN.local>
2019-06-06 13:16:09 +0900

    TemplateService仮作成

--- src/jp/co/central_soft/train2019/wakaba/service/TemplateService.java
+++ src/jp/co/central_soft/train2019/wakaba/service/TemplateService.java
@@ -1,5 +1,37 @@
 package jp.co.central_soft.train2019.wakaba.service;
 
-public class TemplateService {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.servlet.ServletException;
+
+import jp.co.central_soft.train2019.wakaba.dao.TemplateDao;
+import jp.co.central_soft.train2019.wakaba.dto.TemplateDto;
+
+public class TemplateService
+{
+	public TemplateService()
+	{
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public List<TemplateDto> getSortedTemplateList(
+			int addressTypeID, int purposeTypeID, List<String> Keyword, int mashiMashiValue)
+					throws ServletException
+	{
+		// TODO ソート済みのテンプレートリストを返すメソッドを実装する
+		List<TemplateDto> list = null;
+
+		try(Connection con = null) { // TODO nullはプレースホルダ
+			TemplateDao dao = new TemplateDao(con);
+			list = dao.selectAll();
+		} catch(SQLException e) {
+			throw new ServletException(e);
+		}
+
+		list.stream().sorted((x, y) ->
+			Math.abs(x.getMashiMashiValue() - mashiMashiValue) - Math.abs(y.getMashiMashiValue() - mashiMashiValue));
+		return list;
+	}
 }
