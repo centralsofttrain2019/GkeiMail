233a7aa30a524750350ff2b37ff2563e5a6c15c3
koki.ando <koki.ando@CS-506.CSRN.local>
2019-06-07 09:51:41 +0900

    SQL更新

---
+++ gkei_mail_db_init.sql
@@ -0,0 +1,63 @@
+INSERT
+INTO AddressType(
+AddressTypeName,
+BuiltInType
+)
+VALUES(
+'上司',
+'BuiltIn'
+);
+
+INSERT
+INTO AddressType(
+AddressTypeName,
+BuiltInType
+)
+VALUES(
+'同僚',
+'BuiltIn'
+);
+
+INSERT
+INTO PurposeType(
+PurposeTypeName,
+MashiMashiType,
+BuiltInType
+)
+VALUES(
+'感謝',
+1,
+'BuiltIn'
+);
+
+INSERT
+INTO PurposeType(
+PurposeTypeName,
+MashiMashiType,
+BuiltInType
+)
+VALUES(
+'謝罪',
+2,
+'BuiltIn'
+);
+
+INSERT
+INTO PurposeType(
+PurposeTypeName,
+MashiMashiType,
+BuiltInType
+)
+VALUES(
+'見舞',
+3,
+'BuiltIn'
+);
+
+INSERT
+INTO Keyword(
+KeywordContent
+)
+VALUES(
+'A'
+);
