eded4ea9e9965fb2bfd0b0196ed51c7d8cbaa430
kazuki.adachi <kazuki.adachi@CS-505.CSRN.local>
2019-06-11 16:09:42 +0900

    AddressTypeDao作成

--- src/jp/co/central_soft/train2019/wakaba/dao/AddressTypeDao.java
+++ src/jp/co/central_soft/train2019/wakaba/dao/AddressTypeDao.java
@@ -1,15 +1,52 @@
 package jp.co.central_soft.train2019.wakaba.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import jp.co.central_soft.train2019.wakaba.domain.BuiltInTypeEnum;
 import jp.co.central_soft.train2019.wakaba.dto.AddressTypeDto;
 
 public class AddressTypeDao
 {
+	private Connection con;
+
+	private static final String FIND_ALL =
+			"SELECT * FROM AddressType";
+
+	public AddressTypeDao(Connection con) {
+		this.con = con;
+	}
+
 	public List<AddressTypeDto> findAll() throws SQLException
 	{
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		List<AddressTypeDto> adrList = new ArrayList<AddressTypeDto>();
+		PreparedStatement ps = this.con.prepareStatement(FIND_ALL);
+
+		try (ResultSet rs = ps.executeQuery())
+		{
+			while(rs.next())
+			{
+				AddressTypeDto adr = new AddressTypeDto();
+				adr.setAddressTypeID(rs.getInt("AddressTypeID"));
+				adr.setAddressTypeName(rs.getString("AddressTypeName"));
+				adr.setBuiltInType( BuiltInTypeEnum.valueOf( rs.getString("BuiltInType") ));
+				adr.setCreateDate( LocalDateTime.of(
+						rs.getDate("createDate").toLocalDate(),
+						rs.getTime("createDate").toLocalTime()
+						) );
+//				adr.setSuperType();
+				adrList.add(adr);
+			}
+		}
+		catch (SQLException e)
+		{
+			throw e;
+		}
+		return adrList;
 	}
 }
