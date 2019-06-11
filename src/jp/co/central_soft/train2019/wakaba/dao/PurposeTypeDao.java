ff86606c97cc500df9115bf2da16e959641cdfda
koki.ando <koki.ando@CS-506.CSRN.local>
2019-06-11 13:24:31 +0900

    DAOとServiceのスタブ作成 (1-1)

---
+++ src/jp/co/central_soft/train2019/wakaba/dao/PurposeTypeDao.java
@@ -0,0 +1,15 @@
+package jp.co.central_soft.train2019.wakaba.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import jp.co.central_soft.train2019.wakaba.dto.PurposeTypeDto;
+
+public class PurposeTypeDao
+{
+	public List<PurposeTypeDto> findAll() throws SQLException
+	{
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+}
