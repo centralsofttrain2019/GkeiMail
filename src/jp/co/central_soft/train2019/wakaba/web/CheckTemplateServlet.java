20213c1de8fb62e9cedf4d20a42db257183357d0
kazuho.tsuri <kazuho.tsuri@CS-524.CSRN.local>
2019-06-12 15:00:35 +0900

    テンプレート確認画面のサーブレットできたぉ

---
+++ src/jp/co/central_soft/train2019/wakaba/web/CheckTemplateServlet.java
@@ -0,0 +1,57 @@
+package jp.co.central_soft.train2019.wakaba.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.central_soft.train2019.wakaba.bean.CheckTemplateBean;
+import jp.co.central_soft.train2019.wakaba.dto.TemplateDto;
+import jp.co.central_soft.train2019.wakaba.service.TemplateService;
+
+/**
+ * Servlet implementation class DispTemplateServlet
+ */
+@WebServlet("/CheckTemplateServlet")
+public class CheckTemplateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckTemplateServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int app = Integer.parseInt(request.getParameter("app"));
+
+		TemplateService service = new TemplateService();
+		CheckTemplateBean bean = new CheckTemplateBean();
+
+		TemplateDto dto = service.getTemplateByKey(app);
+		bean.setTemplateContent(dto.getTemplateContent());
+
+		request.setAttribute("bean", bean);
+		RequestDispatcher disp = request.getRequestDispatcher("/checkTemplate.jsp");
+		disp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
