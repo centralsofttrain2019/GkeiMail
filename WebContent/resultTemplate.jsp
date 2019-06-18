37aac597356bd20af0d7f5491d35c081082c1749
kazuho.tsuri <kazuho.tsuri@CS-524.CSRN.local>
2019-06-18 14:05:53 +0900

    Revert "なにこれ"
    This reverts commit cfee953302dfa4be516de9f2721efbe2ce92644e.

---
+++ WebContent/resultTemplate.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <!-- jsp:useBean id="bean" class="bean.Bean" scope="request" -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>テンプレート生成確認画面</title>
+</head>
+<body>
+このテンプレートでよろしいですか？
+<button type="submit" name="name" >OK</button>
+<button type="submit" name="name" >やり直し</button>
+<br>
+<div style="display:inline-block; padding:15px; border:1px solid; border-radius:10px; word-break:break-all;">
+ここにテンプレート<br>
+本文<br>
+本文</div>
+</body>
+</html>
---
+++ src/jp/co/central_soft/train2019/wakaba/web/ResultTemplateServlet.java
@@ -0,0 +1,59 @@
+package jp.co.central_soft.train2019.wakaba.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DispTemplateServlet
+ */
+@WebServlet("/ResultTemplateServlet")
+public class ResultTemplateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ResultTemplateServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String atesakivalue;
+		String naiyouvalue;
+		String masimasivalue;
+		atesakivalue = request.getParameter("atesaki");
+		naiyouvalue = request.getParameter("naiyou");
+		masimasivalue = request.getParameter("mashimashi");
+		System.out.print(atesakivalue + naiyouvalue + masimasivalue);
+		//String template = request.getParameter("template");
+
+		//Bean bean = new Bean();
+		//bean.setMsg(template);
+
+		//beanをリクエストにセット キー名は「bean」とする
+		//request.setAttribute("bean", bean);
+
+		//JSPに遷移する
+		RequestDispatcher disp = request.getRequestDispatcher("/resultTemplate.jsp");
+		disp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
