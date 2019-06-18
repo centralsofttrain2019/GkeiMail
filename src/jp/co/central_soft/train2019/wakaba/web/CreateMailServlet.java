867d5ad3083ddd9aae8e3676ca0997046f6ef7fd
kazuki.adachi <kazuki.adachi@CS-505.CSRN.local>
2019-06-18 13:34:30 +0900

    遷移先を修正

--- src/jp/co/central_soft/train2019/wakaba/web/CreateMailServlet.java
+++ src/jp/co/central_soft/train2019/wakaba/web/CreateMailServlet.java
@@ -9,40 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CreateMailServlet
- */
 @WebServlet("/CreateMailServlet")
 public class CreateMailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreateMailServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException
 	{
-		RequestDispatcher rd = request.getRequestDispatcher("/index.html");
+		RequestDispatcher rd = request.getRequestDispatcher("/createMail.jsp");
 		rd.forward(request, response);
-//		RequestDispatcher rd = request.getRequestDispatcher("/mailSakusei.jsp");
-//		rd.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
