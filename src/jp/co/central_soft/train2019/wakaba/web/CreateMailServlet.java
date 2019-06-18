f5dba1f5a1dd24393442c264f99d8f3e39ac429c
kazuho.tsuri <kazuho.tsuri@CS-524.CSRN.local>
2019-06-18 15:23:58 +0900

    送信するときのサーブレット

--- src/jp/co/central_soft/train2019/wakaba/web/CreateMailServlet.java
+++ src/jp/co/central_soft/train2019/wakaba/web/CreateMailServlet.java
@@ -15,14 +15,30 @@ public class CreateMailServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException
-	{
+			throws ServletException, IOException {
+
+		String button = request.getParameter("button");
+		if(button.equals("send")) {
+			String atesaki = request.getParameter("atesaki");
+			String kenmei = request.getParameter("kenmei");
+			String honbun = request.getParameter("honbun");
+
+			//MailService service = new MailService();
+			//service.sendMail(atesaki,kenmei,honbun,1);
+
+			RequestDispatcher rd = request.getRequestDispatcher("/DisplayTemplateListServlet");
+			rd.forward(request, response);
+		}
+
+
+
 		RequestDispatcher rd = request.getRequestDispatcher("/createMail.jsp");
 		rd.forward(request, response);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
