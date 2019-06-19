50bed10350eb761d28e0d9d461c56c357819496d
koki.ando <koki.ando@CS-506.CSRN.local>
2019-06-19 12:35:15 +0900

    メール一覧画面のBean作成、DTO仮作成

---
+++ src/jp/co/central_soft/train2019/wakaba/bean/DisplayMailListBean.java
@@ -0,0 +1,55 @@
+package jp.co.central_soft.train2019.wakaba.bean;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisplayMailListBean
+{
+	public class MailInformation {
+		int id;
+		String subject;
+		String from;
+		private LocalDateTime date;
+		public int getId() {
+			return this.id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getSubject() {
+			return this.subject;
+		}
+		public void setSubject(String subject) {
+			this.subject = subject;
+		}
+		public String getFrom() {
+			return this.from;
+		}
+		public void setFrom(String from) {
+			this.from = from;
+		}
+		public LocalDateTime getDate() {
+			return this.date;
+		}
+		public void setDate(LocalDateTime date) {
+			this.date = date;
+		}
+		public String getJoinedString() {
+			return this.getSubject() + " / " + this.getFrom() + " / " + this.getDate();
+		}
+	}
+	private List<MailInformation> mailInformations = new ArrayList<>();
+	public void putFromDtoToMail(MailDto dto) {
+		MailInformation entry = new MailInformation();
+		entry.setId(dto.getId());
+		entry.setSubject(dto.getSubject());
+		entry.setFrom(dto.getFrom());
+		entry.setDate(dto.getDate());
+		this.mailInformations.add(entry);
+	}
+	public List<MailInformation> getMailInformations() {
+		return this.mailInformations;
+	}
+}
+
---
+++ src/jp/co/central_soft/train2019/wakaba/bean/MailDto.java
@@ -0,0 +1,28 @@
+package jp.co.central_soft.train2019.wakaba.bean;
+
+import java.time.LocalDateTime;
+
+public class MailDto
+{
+	// TODO 未完成
+
+	public int getId() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
+	public String getSubject() {
+		// TODO 自動生成されたメソッド・スタブ
+		return "けんめい";
+	}
+
+	public String getFrom() {
+		// TODO 自動生成されたメソッド・スタブ
+		return "きみ";
+	}
+
+	public LocalDateTime getDate() {
+		// TODO 自動生成されたメソッド・スタブ
+		return LocalDateTime.now();
+	}
+}
