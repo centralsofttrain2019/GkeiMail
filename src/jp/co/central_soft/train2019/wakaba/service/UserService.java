c34bcab434673535b6a86def3bf7aa63e3dea4a9
kazuki.adachi <kazuki.adachi@CS-505.CSRN.local>
2019-06-20 11:23:54 +0900

    User関係のクラスの作成、追加

---
+++ src/jp/co/central_soft/train2019/wakaba/service/UserService.java
@@ -0,0 +1,12 @@
+package jp.co.central_soft.train2019.wakaba.service;
+
+import jp.co.central_soft.train2019.wakaba.dto.UserDto;
+
+public class UserService {
+
+	public UserDto getUserByName(String userID, String password) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
