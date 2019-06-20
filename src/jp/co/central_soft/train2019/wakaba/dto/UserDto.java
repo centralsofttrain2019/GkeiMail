c27130229b126dada1b5783f6dfd2cf414c0a1d6
taichi.ugusa <taichi.ugusa@CS-510.CSRN.local>
2019-06-20 15:32:10 +0900

    ログイン機能の修正/宇草太一

--- src/jp/co/central_soft/train2019/wakaba/dto/UserDto.java
+++ src/jp/co/central_soft/train2019/wakaba/dto/UserDto.java
@@ -1,5 +1,106 @@
 package jp.co.central_soft.train2019.wakaba.dto;
 
 public class UserDto {
+	private String	userID	;
+	private String	firstName	;
+	private String	lastName	;
+	private String	gender	;
+	private String	occupation	;
+	private String	organization	;
+	private String	depeartment	;
+	private String	signature	;
+	private String	password	;
+	private String	mailAddress	;
+	private String	usercol	;
+	private String	user	;
+
+	private String	userName;		// VARCHAR(45) ,
+	private String	userPassword;	// VARCHAR(45) ,
+
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	public String getOrganization() {
+		return organization;
+	}
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+	public String getDepeartment() {
+		return depeartment;
+	}
+	public void setDepeartment(String depeartment) {
+		this.depeartment = depeartment;
+	}
+	public String getSignature() {
+		return signature;
+	}
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public String getUsercol() {
+		return usercol;
+	}
+	public void setUsercol(String usercol) {
+		this.usercol = usercol;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
 
 }
